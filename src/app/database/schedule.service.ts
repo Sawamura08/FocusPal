e859bc34d76aad4dc99984f75057bcbaec6a9bd1
Mark Julius Hernandez <hernandezmarkjulius24@gmail.com>
2024-10-02 15:12:17 +0800

    Fix the login in getting the date and daily

--- src/app/database/schedule.service.ts
+++ src/app/database/schedule.service.ts
@@ -11,7 +11,9 @@ export class ScheduleService {
   schedList$: Observable<Schedule[]>;
   constructor(private datePipe: DatePipe) {
     /* ----------- QUERY FETCH data from DB REACTIVELY/ ON LIVE */
-    this.schedList$ = from(liveQuery(() => db.schedList.toArray()));
+    this.schedList$ = from(liveQuery(() => this.getAllTaskDefault()));
+    const dateToday = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+    console.log(dateToday);
   }
 
   public getSchedList = (): Observable<Schedule[]> => {
@@ -22,11 +24,11 @@ export class ScheduleService {
     const dateToday = new Date().toISOString().split('T')[0];
     const newSched: Schedule = {
       userId: id,
-      title: 'IT Elective 2',
+      title: 'Break Time',
       date: new Date(),
-      startTime: new Date(dateToday + 'T' + '11:30'),
-      endTime: new Date(dateToday + 'T' + '14:30'),
-      repeat: 4,
+      startTime: new Date(dateToday + 'T' + '16:00'),
+      endTime: new Date(dateToday + 'T' + '16:30'),
+      repeat: 0,
       type: 0,
       isActive: 1,
     };
@@ -41,12 +43,16 @@ export class ScheduleService {
   /* GET SCHEDULE BY DATE | REPEAT */
 
   public getAllTaskDefault = async (): Promise<Schedule[]> => {
+    // CHECK THE DATE TODAY ON A 'YYYY-MM-DD' FORMAT
     const dateToday = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
 
     const sched = await db.schedList
       .filter((sched) => {
+        // CHECK it has DATE AND IS ONE TIME SCHED ONLY
         if (sched.date && sched.repeat === 0) {
-          return sched.date.toISOString().includes(dateToday!);
+          const schedDate = this.datePipe.transform(sched.date, 'yyyy-MM-dd');
+          return schedDate === dateToday ? true : false;
+          // CHECK IF THE SCHED IS ON DAILY BASIS
         } else if (sched.repeat === 1) {
           return true;
         } else {
@@ -55,8 +61,28 @@ export class ScheduleService {
       })
       .toArray();
 
+    sched.sort((a, b) => {
+      const converted: number[] = this.getTimeBySched(a.startTime, b.startTime);
+      return converted[0] - converted[1];
+    });
+
     return sched;
   };
 
   /* END OF GET SCHEDULE BY DATE | REPEAT */
+
+  /* CONVERT DATE TIME TO TIME ONLY AND CHECK THE DIFFERENCE */
+
+  private getTimeBySched = (firstDate: Date, secondDate: Date) => {
+    const firstHour = firstDate.getHours();
+    const firstMinutes = firstDate.getMinutes();
+    const first: number = firstHour * 60 + firstMinutes;
+
+    const seoncdHour = secondDate.getHours();
+    const secondMinutes = secondDate.getMinutes();
+    const second: number = seoncdHour * 60 + secondMinutes;
+
+    return [first, second] as [number, number];
+  };
+  /* END */
 }
