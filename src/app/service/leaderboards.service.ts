f7c44a7a28f2a43a2d826b663319d1d05de37a6a
Mark Julius Hernandez <hernandezmarkjulius24@gmail.com>
2024-12-31 12:00:03 +0800

    complete the fetch in leaderboard

--- src/app/service/leaderboards.service.ts
+++ src/app/service/leaderboards.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, EMPTY, Observable, of } from 'rxjs';
 import { ResponseService } from './reponse.service';
 import { userCreation, userLeaderboard } from '../interfaces/auth.inferface';
 import { updateRequest } from '../views/leaderboards/model/leaderboardClass';
@@ -30,6 +30,18 @@ export class LeaderboardsService {
     data: updateRequest
   ): Observable<userLeaderboard> => {
     return this.http.patch<userLeaderboard>(this.updateUrl, data).pipe(
+      catchError((err) => {
+        this.response.errorResponse();
+        return of(err);
+      })
+    );
+  };
+
+  private readonly topUsersUrl =
+    'http://192.168.1.10:7242/api/Leaderboard/GetLeaderboardTop';
+
+  public fetchTopUsers = (): Observable<userLeaderboard[]> => {
+    return this.http.get<userLeaderboard[]>(this.topUsersUrl).pipe(
       catchError((err) => {
         this.response.errorResponse();
         return of(err);
--- src/app/views/leaderboards/leaderboards.component.ts
+++ src/app/views/leaderboards/leaderboards.component.ts
@@ -1,25 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { headerType } from '../../components/header/header.component';
 import {
   BackgroundSyncService,
   syncType,
 } from '../../service/background-sync.service';
+import { userLeaderboard } from '../../interfaces/auth.inferface';
+import { LeaderboardsService } from '../../service/leaderboards.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-leaderboards',
   templateUrl: './leaderboards.component.html',
   styleUrl: './leaderboards.component.scss',
 })
-export class LeaderboardsComponent {
-  constructor(protected backgroundSync: BackgroundSyncService) {}
+export class LeaderboardsComponent implements OnInit {
+  constructor(
+    protected backgroundSync: BackgroundSyncService,
+    protected leader: LeaderboardsService
+  ) {}
 
+  ngOnInit(): void {
+    this.getTopUsers();
+  }
   public homeHeaderData: headerType = {
     type: 'Leaderboards',
     isHome: true,
   };
+  public destroyRef = inject(DestroyRef);
 
   isOtherLeaderShowed: boolean = false;
   public showOtherLeaders = () => {
     this.isOtherLeaderShowed = !this.isOtherLeaderShowed;
+  };
+
+  public topUsers: userLeaderboard[] = [];
+
+  public getTopUsers = () => {
+    this.leader
+      .fetchTopUsers()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((value) => {
+        this.topUsers = value;
+      });
+  };
+
+  public fetchTopUserProfile = (index: number) => {
+    if (this.topUsers != undefined) {
+      return `Avatars/${this.topUsers[index].profile}`;
+    } else {
+      return '';
+    }
+  };
+
+  public fetchOtherTopUser = () => {
+    return this.topUsers.slice(3);
   };
 }
